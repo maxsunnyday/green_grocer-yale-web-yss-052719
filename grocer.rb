7aa367861372c1640896723df6d64ad8fb9fbb3f
Max Sun <max.sun@yale.edu>
2019-05-20 22:02:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,42 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each do |big_hash|
+    big_hash.each do |item, hash|
+      new_hash = hash
+      count = 1
+      new_hash[:count] = count
+      if new_cart.keys.include?(item)
+        new_cart[item][:count] += 1
+      else
+        new_cart[item] = new_hash
+      end
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  cart.each do |item, hash|
+    new_hash = hash
+    coupons.each do |coupon|
+      if item == coupon[:item]
+        count = 0
+        if new_hash[:count] >= coupon[:num]
+          count += 1
+          new_cart["#{item} W/COUPON"] = {
+            price: coupon[:cost],
+            clearance: new_hash[:clearance],
+            count: count
+          }
+          new_hash[:count] -= coupon[:num]
+        end
+      end
+    end
+    new_cart[item] = new_hash
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
