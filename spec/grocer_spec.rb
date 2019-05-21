107c6a623e63640913f7f1e910fb1cc14fbab816
Max Sun <max.sun@yale.edu>
2019-05-21 04:33:46 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -84,7 +84,7 @@ describe "Grocer" do
 
       it "accounts for when there are more items than the coupon allows" do
         cheese = find_item('CHEESE')
-        cart = Array.new(3, cheese)
+        cart = Array.new(5, cheese)
         consolidated_cart = consolidate_cart(cart)
         cheese_coupon = find_coupon("CHEESE")
 
